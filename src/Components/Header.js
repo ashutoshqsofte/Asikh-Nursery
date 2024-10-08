54412b071c67516b360728ded0ae7fbe5782c493
ashutoshqsofte <ashutosh@qsofte.com>
2024-10-08 10:10:12 +0530

    first changes

---
+++ src/Components/Header.js
@@ -0,0 +1,25 @@
+import "./Head.css";
+
+function Header() {
+  return (
+    <div className="head">
+      <a className="logo" href="*/">
+        Asikh Nursery
+      </a>
+      <a className="sections" href="*/">
+        Home
+      </a>
+      <a className="sections" href="*/">
+        Plant care
+      </a>
+      <a className="sections" href="*/">
+        About us
+      </a>
+      <a className="sections" href="*/">
+        Contact Us
+      </a>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/Pages/Home.js
@@ -0,0 +1,15 @@
+import Header from "../Components/Header";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <div className="img-main">
+        <h1 className="txt">Fresh Product Delivery</h1>
+      </div>
+    </>
+  );
+};
+
+export default Home;
