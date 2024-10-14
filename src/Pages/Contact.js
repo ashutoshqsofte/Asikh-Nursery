bd16191bac7015961c45a309f9ab810d98e9f4fb
ashutoshqsofte <ashutosh@qsofte.com>
2024-10-14 13:54:36 +0530

    done for laptop

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -4,6 +4,51 @@ const Contact = () => {
   return (
     <>
       <Header />
+      <div className="cntct-main">
+        <div className="img-cntct">
+          <img
+            className="cntct-img"
+            src={"./images/flowers.jfif"}
+            alt="description"
+          />
+        </div>
+        <div className="cntct-info">
+          <div className="Fst-ph">
+            <img
+              className="smlimgP-cnt"
+              src={"./images/Phone_icon.png"}
+              alt="description"
+            />
+            <p>+91 913587xxxx</p>
+          </div>
+          <div>
+            <img
+              className="smlimgP-cnt"
+              src={"./images/mail.png"}
+              alt="description"
+            />
+            <p>asikh@gmail.com</p>
+          </div>
+
+          <div>
+            <a href="*">
+              <img
+                className="smlimgP-cnt-sc"
+                src={"./images/fb.png"}
+                alt="description"
+              />
+            </a>
+
+            <a href="*">
+              <img
+                className="smlimgP-cnt-sc"
+                src={"./images/insta2.png"}
+                alt="description"
+              />
+            </a>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
--- src/Pages/Plant.js
+++ src/Pages/Plant.js
@@ -4,6 +4,57 @@ const Plant = () => {
   return (
     <>
       <Header />
+      <div className="plntpag">
+        <div className="txtnmP">
+          <h1 className="headP">Plant Care:</h1>
+          <p className="animatePtxt">
+            To care for a plant effectively, ensure it receives the right amount
+            of sunlight based on its needs, and water it regularly without
+            overwatering. Use well-draining soil and provide appropriate
+            nutrients through fertilizer during the growing season.
+            Additionally, periodically check for pests and diseases, and prune
+            any dead or damaged leaves to promote healthy growth.
+          </p>
+        </div>
+        <div>
+          <img
+            className="smlimgP"
+            src={"./images/Asparagus(plant).jfif"}
+            alt="description"
+          />
+        </div>
+      </div>
+
+      <div className="plntpag2">
+        <div className="txtnmP2">
+          <h1 className="headP2">Points-</h1>
+          <p className="animatePtxt2">
+            Sunlight: Most plants need sunlight to thrive, but the amount
+            varies. Some prefer full sun, while others do well in partial or
+            indirect light. Make sure you know the light needs of your specific
+            plant and place it accordingly.
+          </p>
+          <p className="animatePtxt2">
+            Watering: Water your plants based on their individual needs. Some
+            plants like to dry out between waterings, while others need
+            consistently moist soil. Avoid overwatering as it can lead to root
+            rot.
+          </p>
+          <p className="animatePtxt2">
+            Soil: Healthy soil is the foundation of plant growth. Use
+            well-drained, nutrient-rich soil suitable for the type of plant
+            you're growing. Adding compost can boost the soil’s quality and
+            promote better growth.
+          </p>
+        </div>
+        <div>
+          <img
+            className="smlimgP2"
+            src={"./images/plnt2.jfif"}
+            alt="description"
+          />
+        </div>
+      </div>
     </>
   );
 };
